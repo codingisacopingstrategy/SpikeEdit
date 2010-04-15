87f2f7948edbc49de06671dc90961d430dd43e19
brendon <brendon@spike.net.nz>
2010-04-16 11:48:36 +1200

    Added some CSS styling to make the identification and selection of an editable area more obvious

--- spike_edit.js
+++ spike_edit.js
@@ -1,5 +1,12 @@
 jQuery(function($) {
 	$(".editable").each(function() {
 		this.contentEditable = true;
+		$(this).addClass("spikeEditable");
+		$(this).focus(function() {
+			
+		});
+		$(this).blur(function() {
+			
+		});
 	});
 });
