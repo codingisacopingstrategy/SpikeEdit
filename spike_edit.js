1d969463c04d6014bec36668a152f460a55fe12b
brendon <brendon@spike.net.nz>
2010-04-16 15:40:32 +1200

    Added a rudimentary toolbar and commented the code so far

--- spike_edit.js
+++ spike_edit.js
@@ -1,4 +1,7 @@
 jQuery(function($) {
+	
+	// Loop through all elements with the class .editable and make them editable
+	// TODO: We might want to limit these elements to div's only
 	$(".editable").each(function() {
 		this.contentEditable = true;
 		$(this).addClass("spikeEditable");
@@ -6,7 +9,17 @@ jQuery(function($) {
 			
 		});
 		$(this).blur(function() {
+			// This will help us debug the code that a browser generates when we do something
 			$("#debug").text($(this).html());
 		});
 	});
+	
+	// Create the toolbar at the top of the screen
+	$("body").append($("<div id='spikeToolbar'></div>"));
+	$("#spikeToolbar").append($("<a href='#'>Bold</a>"))
+					  .append(" | ")
+					  .append($("<a href='#'>Italic</a>"))
+					  .append(" | ")
+					  .append($("<a href='#'>Underline</a>"));
+
 });
