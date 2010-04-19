cd2ef7f505abfd87391e843e490f8d9242910b57
brendon <brendon@spike.net.nz>
2010-04-19 17:16:29 +1200

    Fixed the focus problem after execCommand for Firefox

--- spike_edit.js
+++ spike_edit.js
@@ -1,32 +1,5 @@
 jQuery(function($) {
 	
-	// Loop through all elements with the class .editable and make them editable
-	// TODO: We might want to limit these elements to div's only
-	$(".editable").each(function() {
-		this.contentEditable = true;
-		$(this).addClass("spikeEditable");
-		$(this).focus(function() {
-			
-		});
-		$(this).blur(function() {
-			// This will help us debug the code that a browser generates when we do something
-			$("#debug").text($(this).html());
-		});
-	});
-	
-	// Create toolbar buttons
-	bold_button = $("<a href='#'>Bold</a>").click(function() {
-		document.execCommand("bold", false, null);
-	});
-
-	// Create the toolbar at the top of the screen
-	$("body").append($("<div id='spikeToolbar'></div>"));
-	$("#spikeToolbar").append(bold_button)
-					  .append(" | ")
-					  .append($("<a href='#'>Italic</a>"))
-					  .append(" | ")
-					  .append($("<a href='#'>Underline</a>"));
-
 	// A function to clean up HTML generated
 	function cleanHTMLOutput() {
 		$(".editable").each(function() {
@@ -36,5 +9,39 @@ jQuery(function($) {
 			});
 		});
 	}
+	
+	// A function to make execCommand cross-browser identical
+	function spikeExecCommand(commandName, showDefaultUI, valueArgument) {
+		document.execCommand(commandName, showDefaultUI, valueArgument);
+		// Mozilla needs to be refocused on the current editable area
+		if ($.browser.mozilla) { current_focused_editable.focus(); }
+	}
+	
+	// Loop through all elements with the class .editable and make them editable
+	// TODO: We might want to limit these elements to div's only
+	$(".editable").each(function() {
+		this.contentEditable = true;
+		$(this).addClass("spikeEditable");
+		$(this).focus(function() {
+			current_focused_editable = $(this);
+		});
+		$(this).blur(function() {
+			// This will help us debug the code that a browser generates when we do something
+			$("#debug").text($(this).html());
+		});
+	});
+	
+	// Create toolbar buttons
+	bold_button = $("<a href='#'>Bold</a>").click(function() {
+		spikeExecCommand("bold", false, null);
+	});
+
+	// Create the toolbar at the top of the screen
+	$("body").append($("<div id='spikeToolbar'></div>"));
+	$("#spikeToolbar").append(bold_button)
+					  .append(" | ")
+					  .append($("<a href='#'>Italic</a>"))
+					  .append(" | ")
+					  .append($("<a href='#'>Underline</a>"));
 	
 });
